27d57b338768b58a7a6bea78348585d0596e2558
Avraham Appel <avraham.appel@humi.ca>
2024-02-20 12:35:23 -0500

    Pin old pkgs for payroll

--- flake.nix
+++ flake.nix
@@ -3,19 +3,25 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
+    nixpkgs-libxml-2-11.url = "nixpkgs/b0d36bd0a420ecee3bc916c91886caca87c894e9";
   };
 
   outputs =
     { self
     , nixpkgs
+    , nixpkgs-libxml-2-11
     , flake-utils
     , ...
     }:
     flake-utils.lib.eachDefaultSystem (system:
     let
-      pkgs = import nixpkgs {
-        inherit system;
-      };
+      pkgs = import nixpkgs { inherit system; };
+
+      # nokogiri is currently blocked from compiling on nixos unstable
+      # [d870193](https://github.com/NixOS/nixpkgs/pull/274550/commits/d870193b2d99dc2744cee8111468135e4c83bde2)
+      # and above due to changes in libxml2 which cause compiler errors.
+      # This is solved in Nokogiri v1.16.0.rc1 and above. See https://github.com/sparklemotion/nokogiri/issues/3071
+      oldPkgs = import nixpkgs-libxml-2-11 { inherit system; };
 
       # for angular templates
       # To update this, cd into ./ngserver and run
@@ -31,8 +37,8 @@
       }).nodeDependencies;
 
       humix = pkgs.callPackage ./humix.nix {
-        projects = import ./projects.nix {
-          inherit pkgs ngserver stylelint-lsp;
+        projects = pkgs.callPackage ./projects.nix { } {
+          inherit ngserver oldPkgs stylelint-lsp;
         };
       };
     in
--- projects.nix
+++ projects.nix
@@ -1,4 +1,6 @@
-{ pkgs, ngserver, stylelint-lsp }:
+{ pkgs }:
+
+{ ngserver, oldPkgs, stylelint-lsp }:
 
 #########################################
 # Main project applications config list #
@@ -168,7 +170,7 @@ in
         copy = true;
         text = /* nix */ ''
           let
-            pkgs = import ${pkgs.path} { };
+            pkgs = import ${oldPkgs.path} { };
             ruby = pkgs.ruby_3_1;
             env = pkgs.bundlerEnv {
               name = "payroll-env";
@@ -183,9 +185,6 @@ in
                   dontBuild = false;
                   patches = [ ${./patches/rbtree-fix-clang-16.patch} ];
                 };
-                # nokogiri is currently blocked from compiling on nixos unstable [d870193](https://github.com/NixOS/nixpkgs/pull/274550/commits/d870193b2d99dc2744cee8111468135e4c83bde2) and above
-                # due to changes in libxml2 which cause compiler errors. If our dependency is not updated soon, we'll have to disable the compiler error
-                # This is solved in Nokogiri v1.16.0.rc1 and above. See https://github.com/sparklemotion/nokogiri/issues/3071
               };
               ignoreCollisions = true;
               extraConfigPaths = [ "''${./.}/engines" ];
