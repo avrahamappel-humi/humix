ba85e9b19b2ec8eeb7751f7c6ceeb80786b4477f
Avraham Appel <aappel248@gmail.com>
2024-09-04 00:24:48 -0400

    Build templates locally
    Failing due to SSL / netskope

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -156,6 +156,63 @@ in
       '';
     }
 
+    # avante.nvim dependencies
+    vimPlugins.nui-nvim
+    vimPlugins.dressing-nvim
+    {
+      plugin = pkgs-unstable.vimPlugins.render-markdown;
+      type = "lua";
+      config = /* lua */ ''
+        require('render-markdown').setup {
+          file_types = { 'Avante' },
+        }
+      '';
+    }
+
+    # Avante
+    {
+      plugin = vimUtils.buildVimPlugin {
+        pname = "avante.nvim";
+        version = srcs."avante.nvim".version;
+        src = srcs."avante.nvim";
+        # Fails with SSL error from netskope
+        buildPhase = ''
+          export CARGO_HOME=./cargo
+          mkdir $CARGO_HOME
+          BUILD_FROM_SOURCE=true make
+        '';
+        buildInputs = with pkgs; [
+          cargo
+          oniguruma
+          openssl
+          darwin.apple_sdk.frameworks.Security
+        ];
+        nativeBuildInputs = with pkgs; [
+          pkg-config
+        ];
+      };
+      type = "lua";
+      config = /* lua */ ''
+        require('avante_lib').load()
+        require('avante').setup {
+          provider = "openai",
+          openai = {
+            api_key_name = 'cmd:${open-ai-key-cmd}'
+          },
+          mappings = {
+            ask = "<leader>oa",
+            edit = "<leader>oe",
+            refresh = "<leader>or",
+            toggle = {
+              default = "<leader>ot",
+              debug = "<leader>od",
+              hint = "<leader>oh",
+            },
+          },
+        }
+      '';
+    }
+
     {
       # Linear
       plugin = vimUtils.buildVimPlugin {
