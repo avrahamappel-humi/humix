9883589a8ed79a80e314f27bf7f20b0ece793c2d
Avraham Appel <aappel248@gmail.com>
2024-09-04 00:26:48 -0400

    Remove avante for now

--- home-manager.nix
+++ home-manager.nix
@@ -169,44 +169,6 @@ in
         }
       '';
     }
-    
-    {
-      plugin = vimUtils.buildVimPlugin {
-        pname = "avante.nvim";
-        version = srcs."avante.nvim".version;
-        src = srcs."avante.nvim";
-      };
-      type ="lua";
-      config = /* lua */ ''
-        require('avante_lib').load()
-        require('avante').setup {
-          provider = "openai",
-          openai = {
-            api_key_name = 'cmd:${open-ai-key-cmd}'
-          },
-          mappings = {
-            ask = "<leader>oa",
-            edit = "<leader>oe",
-            refresh = "<leader>or",
-            toggle = {
-              default = "<leader>ot",
-              debug = "<leader>od",
-              hint = "<leader>oh",
-            },
-          },
-        }
-      '';
-    }
-    {
-      # Used by avante.nvim
-      plugin = pkgs-unstable.vimPlugins.render-markdown;
-      type = "lua";
-      config = /* lua */ ''
-        require('render-markdown').setup {
-          file_types = { 'Avante' },
-        }
-      '';
-    }
 
     {
       # Linear
